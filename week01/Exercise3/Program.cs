dbb8f58c45d7fb56b647d2309f551289bcb89788
evansnk5 <nkrumahe541.com>
2025-03-10 17:02:39 -0700

    wk1 final assignment

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,31 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+    {        
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        // We could also use a do-while loop here...
+        while (guess != magicNum)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNum > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNum < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,59 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        int number = -1; 
+        
+
+        //create the list
+        List<int> numberList  = new List<int>();
+        Console.WriteLine(" Enter a list of numbers, type 0 when finished.");
+
+        while(number!= 0)
+        {
+            
+              Console.Write("Enter number: ");
+
+              string userNumbers =  Console.ReadLine();
+              number = int.Parse(userNumbers);
+
+              if(number !=0){ 
+                
+                  numberList.Add(number);
+            
+        //  if (number==0)
+        //      break;
+        }
+        }
+        // Part 2: Compute the sum
+             int sum=0;
+
+             foreach(int loopNumber in numberList){
+             sum += loopNumber;
+        }
+           Console.WriteLine($"The sum is {sum}");
+
+        // Part 2: Compute the average
+        float average = ((float)sum)/numberList.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Check for the max number
+         int max = numberList[0];
+
+         foreach (int loopNumber in numberList)
+        {
+            if (loopNumber > max)
+            {
+                // if this number is greater than the max, we have found the new max!
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
+
     }
 }
